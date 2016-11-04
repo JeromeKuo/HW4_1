238a333d5d49adea3f582755f559073d6c733cde
JeromeKuo <Jerome@MacBook-Mao-2.local>
2016-11-04 17:40:54 +0800

    函數練習2 不是5的倍數才相加

---
+++ MyPlayground_HW4_func_2.playground/Contents.swift
@@ -0,0 +1,17 @@
+
+func add_x(start_number start:Int,add_numberadd add:Int,andnumber_end end:Int) ->Int { //定義參數與設定回傳類型
+    var sum = 0
+    for i in start...end {   //建立迴圈範圍
+        
+        if i % 5 != 0 {  //判斷是否為5的倍數(餘數為0) 不為5的倍數才執行迴圈內容
+            sum = sum + i
+            
+        }
+        
+    }
+    
+    return sum //回傳sum值
+    
+}
+
+add_x(start_number: 3, add_numberadd: 5, andnumber_end: 11)  //呼叫函數驗證

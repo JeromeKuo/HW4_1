491dc86b2672f958b7683f5c37b225ccbc8024a3
JeromeKuo <Jerome@MacBook-Mao-2.local>
2016-11-04 18:17:24 +0800

    利用func計算當行>列 時的格內數字總和

---
+++ MyPlayground_HW4_func_4.playground/Contents.swift
@@ -0,0 +1,18 @@
+
+func add_odd(column:Int,row:Int) ->Int{ //設定函數的參數為 列(row) 與 行(column)，並設定回傳類型
+    var sum = 0
+    for i in 0...column {
+        for m in 0...row {
+            
+            if i > m { //若列數為奇數，則執行迴圈內容，進行行列數字的連加
+                sum = sum + m*i
+            }
+            
+        }
+    }
+    
+    return sum //回傳結果至sum
+    
+}
+
+add_odd(column: 7, row: 7) //驗證結果

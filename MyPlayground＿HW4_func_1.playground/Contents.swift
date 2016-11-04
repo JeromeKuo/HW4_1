2d88f1ac20d8db72743789bc26e3329b357262f1
JeromeKuo <Jerome@MacBook-Mao-2.local>
2016-11-04 17:39:30 +0800

    函數練習1  (5的倍數相加)

--- MyPlayground＿HW4_func_1.playground/Contents.swift
+++ MyPlayground＿HW4_func_1.playground/Contents.swift
@@ -1,17 +1,17 @@
 
-func add_x(start_number start:Int,add_numberadd add:Int,andnumber_end end:Int) ->Int {
+func add_x(start_number start:Int,add_numberadd add:Int,andnumber_end end:Int) ->Int { //定義參數與設定回傳類型
     var sum = 0
-    for i in start...end {
+    for i in start...end {   //建立迴圈範圍
         
-        if i % 5 == 0 {
+        if i % 5 == 0 {  //判斷是否為5的倍數(餘數為0)
          sum = sum + i
             
         }
         
     }
     
-   return sum
+   return sum //回傳sum值
     
 }
 
-add_x(start_number: 3, add_numberadd: 5, andnumber_end: 98)
+add_x(start_number: 3, add_numberadd: 5, andnumber_end: 98)  //呼叫函數驗證
